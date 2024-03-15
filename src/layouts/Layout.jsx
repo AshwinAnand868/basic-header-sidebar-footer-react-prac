d3c580e2808f727c5d016822f0e77327557bfa1d
AshwinAnand868 <anandashwin868@gmail.com>
2024-03-15 12:21:59 -0400

    Add description about HOC in the Layout component

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -2,6 +2,12 @@ import Footer from './Footer';
 import Header from './Header';
 import SideBar from './SideBar';
 
+/* 
+    This component is a HOC (High order component) that takes other components as
+    its parameters and renders those components as its children. Its main functionality
+    is that it that it allows us to pass any component based on different routes in 
+    the application and it will update accordingly without affecting its outer structure.
+*/
 const Layout = (Component) => ({...props}) => (
     <div>
         <Header />
